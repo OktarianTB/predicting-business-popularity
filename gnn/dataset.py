b3c45793d0a353181c06e58b5dd67855488dc324
syncdoth <choisehyun98@gmail.com>
2021-05-18 17:07:24 +0800

    update gnn modules

--- gnn/dataset.py
+++ gnn/dataset.py
@@ -13,7 +13,7 @@ def from_networkx(G):
 
     Args:
         G (networkx.Graph or networkx.DiGraph): A networkx graph.
-        
+
     This code is forked from official torch_geometric.utils.convert.from_networkx,
     version 1.7.0
     """
@@ -37,7 +37,7 @@ def from_networkx(G):
                 data[str(key)] = [value]
             else:
                 data[str(key)].append(value)
-    
+
     for key, item in data.items():
         try:
             data[key] = torch.tensor(item)
@@ -52,7 +52,12 @@ def from_networkx(G):
 
 
 class HybridDataset:
-    def __init__(self, res_graph_path, user_graph_path, user_per_res_path, split=[0.8, 0.1, 0.1]):
+
+    def __init__(self,
+                 res_graph_path,
+                 user_graph_path,
+                 user_per_res_path,
+                 split=[0.8, 0.1, 0.1]):
         """
         res_graph_path = "../graphs/restaurants_with_categories.gpickle"
         user_graph_path = "../graphs/2017-2018_user_network.gpickle"
@@ -66,7 +71,7 @@ class HybridDataset:
 
         self.res_idx2node = dict(enumerate(sorted(self.res_G.nodes())))
         self.res_node2idx = {node: idx for idx, node in self.res_idx2node.items()}
-        
+
         print("converting restaurant graph to pyg graph...", end=" ")
         self.res_pyg_graph = from_networkx(self.res_G)  # to pytorch geometric format
         print("done!")
@@ -79,7 +84,7 @@ class HybridDataset:
 
         self.user_idx2node = dict(enumerate(sorted(self.user_G.nodes())))
         self.user_node2idx = {node: idx for idx, node in self.user_idx2node.items()}
-        
+
         print("converting restaurant graph to pyg graph...", end=" ")
         self.user_pyg_graph = from_networkx(self.user_G)  # to pytorch geometric format
         print("done!")
@@ -89,37 +94,45 @@ class HybridDataset:
         self.visited_user_df.set_index("business_id", inplace=True)
         self.visited_user_df["user_ids"] = self.visited_user_df["user_ids"].apply(eval)
 
+        self.max_k = self.visited_user_df["user_ids"].apply(len).max()
+        print(self.max_k)
+
         # split
         self.train_index, self.val_index, self.test_index = self.train_test_split(split)
-        
+
         # features and labels
         self.res_x = torch.stack(self.res_pyg_graph.node_feature, 0)
         self.user_x = torch.stack(self.user_pyg_graph.node_features, 0)
         self.labels = self.res_pyg_graph.node_label.long()
 
-
     def train_test_split(self, split=[0.8, 0.1, 0.1]):
-        train_nodes, test_nodes = train_test_split(
-            np.arange(self.res_pyg_graph.node_label.shape[0]), test_size=split[1] + split[2], shuffle=True)
-        val_nodes, test_nodes = train_test_split(test_nodes, test_size=split[2] / (split[1] + split[2]), shuffle=True)
+        train_nodes, test_nodes = train_test_split(np.arange(
+            self.res_pyg_graph.node_label.shape[0]),
+                                                   test_size=split[1] + split[2],
+                                                   shuffle=True)
+        val_nodes, test_nodes = train_test_split(test_nodes,
+                                                 test_size=split[2] /
+                                                 (split[1] + split[2]),
+                                                 shuffle=True)
 
         return [torch.LongTensor(x) for x in (train_nodes, val_nodes, test_nodes)]
 
-
     def get_visited_users(self, target_res, k=5):
         target_res = [self.res_idx2node[int(res)] for res in target_res]
-        users_per_res = self.visited_user_df.loc[target_res]["user_ids"].tolist()  # list of list
+        users_per_res = self.visited_user_df.loc[target_res]["user_ids"].tolist(
+        )  # list of list
         num_users_per_res = []
         visited_users = []
         for users in users_per_res:
-            users = users[:k]  # sample first k users
+            if k is not None:
+                users = users[:k]  # sample first k users
             num_users_per_res.append(len(users))
             visited_users.extend(users)
-        
+
         visited_users = [self.user_node2idx[user] for user in visited_users]
 
         return num_users_per_res, np.array(visited_users)
-    
+
     def to(self, device):
         self.res_x = self.res_x.to(device)
         self.user_x = self.user_x.to(device)
@@ -128,11 +141,11 @@ class HybridDataset:
     @property
     def num_res_features(self):
         return self.res_pyg_graph.node_feature[0].shape[0]
-    
+
     @property
     def num_user_features(self):
         return self.user_pyg_graph.node_features[0].shape[0]
-    
+
     @property
     def num_class(self):
         return self.res_pyg_graph.node_label.unique().shape[0]
--- gnn/modeling.py
+++ gnn/modeling.py
@@ -9,7 +9,14 @@ import numpy as np
 
 
 class GNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, num_layers=2, model="GraphSage"):
+
+    def __init__(self,
+                 input_size,
+                 hidden_size,
+                 output_size,
+                 num_layers=2,
+                 model="GraphSage",
+                 aggr="mean"):
         super().__init__()
         self.num_layers = num_layers
 
@@ -17,11 +24,10 @@ class GNN(nn.Module):
         self.convs = nn.ModuleList()
         self.convs.append(conv_model(input_size, hidden_size))
         for _ in range(self.num_layers - 1):
-            self.convs.append(conv_model(hidden_size, hidden_size))
+            self.convs.append(conv_model(hidden_size, hidden_size, aggr=aggr))
 
         self.convs.append(conv_model(hidden_size, output_size))
 
-        
     def forward(self, x, adjs):
         # `train_loader` computes the k-hop neighborhood of a batch of nodes,
         # and returns, for each layer, a bipartite graph object, holding the
@@ -52,33 +58,59 @@ class GNN(nn.Module):
 
 
 class HybridNetwork(nn.Module):
-    def __init__(self, res_size, user_size, hidden_size, output_size, num_layers=2, model="GraphSage", k=5):
+
+    def __init__(self,
+                 res_size,
+                 user_size,
+                 hidden_size,
+                 output_size,
+                 num_layers=2,
+                 model="GraphSage",
+                 aggr="mean"):
         super().__init__()
 
-        self.k = k  # number of users for each business
-        self.res_GNN = GNN(res_size, hidden_size, hidden_size, num_layers=2, model="GraphSage")
-        self.user_GNN = GNN(user_size, hidden_size, hidden_size, num_layers=2, model="GraphSage")
-        self.out_layer = nn.Linear(hidden_size * (self.k + 1), output_size)
+        self.res_GNN = GNN(res_size,
+                           hidden_size,
+                           hidden_size,
+                           num_layers=num_layers,
+                           model=model,
+                           aggr=aggr)
+        self.user_GNN = GNN(user_size,
+                            hidden_size,
+                            hidden_size,
+                            num_layers=num_layers,
+                            model=model,
+                            aggr=aggr)
+        self.out_layer = nn.Linear(hidden_size * 2, output_size)
 
     def forward(self, res_x, res_adjs, user_x, user_adj, inverse_idx, num_users_per_res):
         res_x = self.res_GNN(res_x, res_adjs)  # [batch size, hidden size]
         user_x = self.user_GNN(user_x, user_adj)  # [all_unique_users, hidden size]
         user_x = user_x[inverse_idx]  # [all users per network, hidden size]
-        user_x = torch.split(user_x, num_users_per_res)  # [batch size, # of user per restaruant, hidden size]
-        user_x = pad_sequence(user_x, batch_first=True, padding_value=0.0)  # [batch size, k, hidden size]
-        user_x = torch.reshape(user_x, (user_x.shape[0], -1))  # [batch size, k * hidden size]
-
-        emb = torch.cat([res_x, user_x], 1)  # [batch size, hidden size * (k + 1)]
+        user_x = torch.split(
+            user_x,
+            num_users_per_res)  # batch size * [# of user per restaruant, hidden size]
+        user_x = torch.stack([x.mean(dim=0) for x in user_x],
+                             0)  # [batch size, hidden size]
+        emb = torch.cat([res_x, user_x], 1)  # [batch size, hidden size * 2]
         out = self.out_layer(emb)
         return out
 
 
-def train(model, num_epochs, dataset, train_loader, optimizer, criterion, device="cpu", k=5, all_loader=None):
+def train(model,
+          num_epochs,
+          dataset,
+          train_loader,
+          optimizer,
+          criterion,
+          device="cpu",
+          k=5,
+          all_loader=None):
     for epoch in range(num_epochs):
         model.train()
 
-#         pbar = tqdm(total=int(dataset.train_index.shape[0]))
-#         pbar.set_description(f'Epoch {(epoch + 1):02d}')
+        #         pbar = tqdm(total=int(dataset.train_index.shape[0]))
+        #         pbar.set_description(f'Epoch {(epoch + 1):02d}')
 
         total_loss = total_correct = 0
         steps = 0
@@ -91,18 +123,22 @@ def train(model, num_epochs, dataset, train_loader, optimizer, criterion, device
             unique_users, inverse_idx = np.unique(users, return_inverse=True)
             unique_users = torch.LongTensor(unique_users)
 
-            user_loader = NeighborSampler(dataset.user_pyg_graph.edge_index, node_idx=unique_users,
-                                          sizes=[-1, -1], batch_size=unique_users.shape[0], shuffle=False)
-            
+            user_loader = NeighborSampler(dataset.user_pyg_graph.edge_index,
+                                          node_idx=unique_users,
+                                          sizes=[-1, -1],
+                                          batch_size=unique_users.shape[0],
+                                          shuffle=False)
+
             for num_users, u_id, u_adjs in user_loader:
-                # this is actually just one loop: using for loop since 
+                # this is actually just one loop: using for loop since
                 # next(iter(user_loader)) appears to be buggy
                 u_adjs = [adj.to(device) for adj in u_adjs]
 
             del user_loader
 
             optimizer.zero_grad()
-            out = model(dataset.res_x[n_id], adjs, dataset.user_x[u_id], u_adjs, inverse_idx, num_users_per_res)
+            out = model(dataset.res_x[n_id], adjs, dataset.user_x[u_id], u_adjs,
+                        inverse_idx, num_users_per_res)
             loss = criterion(out, dataset.labels[n_id[:batch_size]])
             loss.backward()
             optimizer.step()
@@ -113,7 +149,8 @@ def train(model, num_epochs, dataset, train_loader, optimizer, criterion, device
             total_correct += int(pred.eq(dataset.labels[n_id[:batch_size]]).sum())
             steps += 1
             num_examples += batch_size
-            
+
+
 #             pbar.set_postfix(accuracy=total_correct / num_examples, curr_loss=total_loss / steps)
 #             pbar.update(batch_size)
 
@@ -125,9 +162,7 @@ def train(model, num_epochs, dataset, train_loader, optimizer, criterion, device
         print(f'Epoch {epoch + 1:02d}, Loss: {loss:.4f}, Approx. Train: {approx_acc:.4f}')
         if all_loader is not None:
             train_acc, val_acc, test_acc = test(model, dataset, all_loader, device, k=k)
-            print(f'Train: {train_acc:.4f}, Val: {val_acc:.4f}, '
-                  f'Test: {test_acc:.4f}')
-
+            print(f'Train: {train_acc:.4f}, Val: {val_acc:.4f}, ' f'Test: {test_acc:.4f}')
 
 
 @torch.no_grad()
@@ -141,17 +176,21 @@ def test(model, dataset, all_loader, device, k=5):
         unique_users, inverse_idx = np.unique(users, return_inverse=True)
         unique_users = torch.LongTensor(unique_users)
 
-        user_loader = NeighborSampler(dataset.user_pyg_graph.edge_index, node_idx=unique_users,
-                                      sizes=[-1, -1], batch_size=unique_users.shape[0], shuffle=False)
+        user_loader = NeighborSampler(dataset.user_pyg_graph.edge_index,
+                                      node_idx=unique_users,
+                                      sizes=[-1, -1],
+                                      batch_size=unique_users.shape[0],
+                                      shuffle=False)
 
         for num_users, u_id, u_adjs in user_loader:
-            # this is actually just one loop: using for loop since 
+            # this is actually just one loop: using for loop since
             # next(iter(user_loader)) appears to be buggy
             u_adjs = [adj.to(device) for adj in u_adjs]
 
         del user_loader
 
-        out = model(dataset.res_x[n_id], adjs, dataset.user_x[u_id], u_adjs, inverse_idx, num_users_per_res)
+        out = model(dataset.res_x[n_id], adjs, dataset.user_x[u_id], u_adjs, inverse_idx,
+                    num_users_per_res)
         outs.append(out.cpu())
 
     outs = torch.cat(outs, dim=0)
